45c9a64a10e4b740b5106ac0742411db2b4bb11b
Rishabh Sareen <rishabh.sareen@dbtechhackathon.com>
2021-04-11 10:58:39 +0530

    check todos functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,15 +3,27 @@ import React from 'react';
 const Todo = ({id,todo,myTodos,setMyTodos}) =>{
     
     const deleteHandler = () => {
+        //create the array with filtering the one having same id
         setMyTodos(myTodos.filter( (e1) => e1.id !== todo.id));
+    };
+
+    const completeHandler = () => {
+        //find index
+        const index=myTodos.findIndex( (e1) => e1.id === todo.id);
+        //copy state
+        const newState=[...myTodos];
+        //change the state
+        newState[index]={...newState[index] , completed:!newState[index].completed};
+        //console.log(index);
+        setMyTodos(newState);
     };
 
     return(
         <div className = 'todo'>
-            <li className="todo-item">
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
                 {todo.content}
             </li>
-            <button className = "complete-btn">
+            <button className = "complete-btn" onClick={completeHandler}>
             <i className="fas fa-check"></i>
             </button>
             <button className = "trash-btn" onClick={deleteHandler}>
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -6,6 +6,7 @@ const Todolist = ({myTodos,setMyTodos}) =>{
 
     return(
         <div className = "todo-conatiner">
+            { myTodos.length===0 ? <div className = "check-todo">No Todos</div>:<div className = "check-todo">Total todos {myTodos.length} </div>}
             <ul className = "todo-list">
             { myTodos.map( (todo) => (
                 <Todo key={todo.id} todo={todo} myTodos={myTodos} setMyTodos={setMyTodos}/>
